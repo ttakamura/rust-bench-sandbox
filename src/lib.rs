ff67f216389608239b781103e969b13337362990
Tatsuya Takamura <takamura@tonchidot.com>
2016-09-14 06:27:30 +0900

    sample bench

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+#![feature(test)]
+extern crate test;
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_add_two(b: &mut Bencher) {
+        b.iter(|| add_two(2));
+    }
+}
